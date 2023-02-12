7afe93d6717a710df03f0f4e1a000ddad395dd7c
JonasLindaas <jonas.drageset.lindaas@gmail.com>
2023-02-12 16:46:25 +0100

    Prototyped html injection.
    Far from finalised, and right now non-interactive.

--- scripts/mana.mjs
+++ scripts/mana.mjs
@@ -135,3 +135,97 @@ class Mana {
 Hooks.once('devModeReady', ({ registerPackageDebugFlag }) => {
     registerPackageDebugFlag(Mana.ID);
 });
+
+// Non-final
+Hooks.on("renderActorSheet", function (dndSheet, html) {
+    let div = document.createElement("div");
+    div.setAttribute("class", "mana-box");
+
+    let label = document.createElement("h2");
+    label.setAttribute("class", "mana-label");
+    label.append("Mana:");
+
+    let currManaInput = document.createElement("input");
+    currManaInput.setAttribute("class", "mana-current");
+    currManaInput.setAttribute("type", "number");
+    currManaInput.setAttribute("name", "TODO"); //TODO: Should be the flag path of the relevant flag 
+    currManaInput.setAttribute("value", "30");
+    currManaInput.setAttribute("data-dtype", "Number");
+    currManaInput.setAttribute("placeholder", "0");
+    currManaInput.setAttribute("title", "Current Mana");
+
+    let maxManaInput = document.createElement("input");
+    maxManaInput.setAttribute("class", "mana-max");
+    maxManaInput.setAttribute("type", "number");
+    maxManaInput.setAttribute("name", "TODO"); //TODO: Should be the flag path of the relevant flag 
+    maxManaInput.setAttribute("value", "120");
+    maxManaInput.setAttribute("data-dtype", "Number");
+    maxManaInput.setAttribute("placeholder", "0");
+    maxManaInput.setAttribute("title", "Maximum Mana Capacity");
+    
+    div.append(label);
+    div.append(currManaInput);
+    div.append(" / ");
+    div.append(maxManaInput);
+
+
+    const inventoryFiltersDiv = html[0].querySelectorAll(".spellbook .inventory-filters");
+    inventoryFiltersDiv[0].prepend(div);
+
+    // The below code injects into the Attributes pane, currently hijacking resources styling, which is buggy and bad.
+    let li = document.createElement("li");
+    li.setAttribute("class", "resource visible");
+
+
+    let h4 = document.createElement("h4");
+    h4.setAttribute("class", "resource-name");
+    
+    let inp0 = document.createElement("input");
+    inp0.setAttribute("name", "system.resources.primary.label");
+    inp0.setAttribute("type", "text");
+    inp0.setAttribute("value", "Mana");
+    inp0.setAttribute("placeholder", "Mana");
+
+    h4.append(inp0);
+
+
+    let liDiv = document.createElement("div");
+    liDiv.setAttribute("class", "resource-value multiple");
+
+    let inp1 = document.createElement("input");
+    inp1.setAttribute("class", "res-value");
+    inp1.setAttribute("type", "text");
+    inp1.setAttribute("name", "TODO"); //TODO: Should be the flag path of the relevant flag 
+    inp1.setAttribute("value", "");
+    inp1.setAttribute("data-dtype", "Number");
+    inp1.setAttribute("placeholder", "0");
+    inp1.setAttribute("title", "Current Mana");
+    inp1.setAttribute("maxlength", "3");
+
+    let sepSpan = document.createElement("span");
+    sepSpan.setAttribute("class", "sep");
+    sepSpan.append(" / ");
+
+    let inp2 = document.createElement("input");
+    inp2.setAttribute("class", "res-max");
+    inp2.setAttribute("type", "text");
+    inp2.setAttribute("name", "TODO"); //TODO: Should be the flag path of the relevant flag 
+    inp2.setAttribute("value", "");
+    inp2.setAttribute("data-dtype", "Number");
+    inp2.setAttribute("placeholder", "0");
+    inp2.setAttribute("title", "Max Mana");
+    inp2.setAttribute("maxlength", "3");
+
+    liDiv.append(inp1);
+    liDiv.append(sepSpan);
+    liDiv.append(inp2);
+
+    li.append(h4);
+    li.append(liDiv);
+
+
+    const attributesResource = html[0].querySelectorAll(".attributes .resources");
+    attributesResource[0].prepend(li);
+
+    Mana.log(true, attributesResource);
+});
