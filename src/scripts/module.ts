e87332535afd6e7cb79769d93cee0f6e3a14f9f3
JonasLindaas <jonas.drageset.lindaas@gmail.com>
2023-10-15 21:42:12 +0200

    Added Vite, TS, and Node.
    Restructured module

---
+++ src/scripts/module.ts
@@ -0,0 +1,7 @@
+// This import is required for styles to be compiled by Vite.
+import "../styles/style.scss";
+
+// TODO
+Hooks.once("init", () => {
+  console.log("Hello world from mana-base!");
+});
